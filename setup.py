911e2f58ffa509fc472842a6913531480eede0bc
Fernando Cesar Serafim <97233963+fernandocesarserafim@users.noreply.github.com>
2024-10-16 16:17:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="fernando",
+    author_email="ferdesigner@hotmail.com",
+    description="meu pacote",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/fernandocesarserafim/simple-package-template/"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
